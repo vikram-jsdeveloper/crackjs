fbd7151275bd818c707eef4efddb00c02d361c1b
vikram-jsdeveloper <jsdeveloper89@gmail.com>
2024-10-06 07:14:14 +0530

    Added responsive menu

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 import './app.scss';
+
+const mobileMenu = document.getElementById('mobile-menu');
+const navMenu = document.querySelector('.nav-menu');
+
+mobileMenu.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
+
+document.addEventListener('click', (event) => {
+  const isClickInside =
+    navMenu.contains(event.target) || mobileMenu.contains(event.target);
+
+  if (!isClickInside) {
+    navMenu.classList.remove('active');
+  }
+});
